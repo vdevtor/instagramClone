c17790323cb8fd666e372592c879b1ee56166b12
vdevtor <vitaoosp@gmail.com>
2021-03-11 15:32:47 -0300

    click profile picture home

--- app/src/main/java/com/example/adapter/PostAdapter.kt
+++ app/src/main/java/com/example/adapter/PostAdapter.kt
@@ -1,19 +1,25 @@
 package com.example.adapter
 
 import android.content.Context
+import android.content.res.Resources
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.fragments.ProfileFragment
 import com.example.instagramclone.R
 import com.example.instagramclone.databinding.PostsLayoutBinding
-import com.example.instagramclone.databinding.UserItemLayoutBinding
 import com.example.model.Post
 import com.example.model.User
-import com.example.utils.Constans.Companion.JaDeiLike
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
@@ -21,7 +27,7 @@ import de.hdodenhof.circleimageview.CircleImageView
 
 class PostAdapter(
     private val mContex: Context,
-    private val mPost: List<Post>
+    private val mPost: List<Post>,
 ) : RecyclerView.Adapter<PostAdapter.ViewHolder>() {
     private val firebaseAuth by lazy {
         Firebase.auth
@@ -32,8 +38,8 @@ class PostAdapter(
     private val db2 by lazy {
         Firebase.firestore.collection("Posts")
     }
-    private var liked : Boolean = false
-    private var r: Boolean = false
+    private var lAU: String = ""
+    private var user_id : String = ""
 
     class ViewHolder(private val binding: PostsLayoutBinding) :
         RecyclerView.ViewHolder(binding.root) {
@@ -48,7 +54,6 @@ class PostAdapter(
         var publisher: TextView = binding.publisher
         var description: TextView = binding.description
         var comments: TextView = binding.comments
-
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostAdapter.ViewHolder {
@@ -63,136 +68,156 @@ class PostAdapter(
         Glide.with(this.mContex).load(post.postimage).into(holder.postImage)
         publisherInfo(holder.profileImage, holder.userName, holder.publisher, post.publisher)
         getDescriptions(holder.description, post.publisher)
-        checkLikesCount(holder.likeButton,holder.likes,post.postid)
+        checkLikesCount(holder.likeButton, holder.likes, post.postid)
         checkLikesBtn(holder.likeButton, holder.likes, post.postid)
-        checkDrawable(holder.likeButton)
+
+        holder.profileImage.setOnClickListener{
+
+            val users = db.document(post.publisher)
+            users.get().addOnSuccessListener {
+                if (it.exists()) {
+                    val user = it.toObject<User>()
+
+                    val pref = mContex.getSharedPreferences("PREFS",Context.MODE_PRIVATE).edit()
+                    pref.putString("profileId",user?.uid)
+                    pref.apply()
+                    (mContex as FragmentActivity).supportFragmentManager.beginTransaction().replace(R.id.fragment_container,
+                        ProfileFragment()
+                    )
+                        .commit()
+                }
+
+                }
+            }
 
 
 
         holder.likeButton.setOnClickListener {
-            checkDrawable(holder.likeButton)
-            if (checkLikesBtn(holder.likeButton, holder.likes, post.postid) && checkDrawable(holder.likeButton)){
-                unlike(holder.likeButton, holder.likes, post.postid)
-                checkLikesCount(holder.likeButton, holder.likes, post.postid)
-                checkLikesBtn(holder.likeButton, holder.likes, post.postid)
+            var ref = db2.whereEqualTo("postid", post.postid)
+            ref.get().addOnSuccessListener {
+                for (document in it) {
+                    var aux = document.reference.collection("Likes")
+                    aux.whereEqualTo("likedby", userUsing).get().addOnSuccessListener {
+                        if (it.isEmpty || it.count() <= 0) {
+                            addLike(post.postid, holder.likeButton)
+                            checkLikesBtn(holder.likeButton, holder.likes, post.postid)
+                            checkLikesCount(holder.likeButton, holder.likes, post.postid)
+                        } else if (!it.isEmpty) {
+                            for (document2 in it) {
+                                var aux1 = document2.get("postid")
+                                var aux2 = document2.get("likedby")
+                                if (aux1 == post.postid) {
+                                    unlike(holder.likeButton, holder.likes, post.postid)
+                                    checkLikesBtn(holder.likeButton, holder.likes, post.postid)
+                                    checkLikesCount(holder.likeButton, holder.likes, post.postid)
 
-
-            }
-
-            else if (!checkLikesBtn(holder.likeButton, holder.likes, post.postid) || !checkDrawable(holder.likeButton)){
-                checkLikesCount(holder.likeButton, holder.likes, post.postid)
-                addLike(post.postid,holder.likeButton)
-                checkLikesBtn(holder.likeButton, holder.likes, post.postid)
-
-
-            }
-
-            checkLikesCount(holder.likeButton, holder.likes, post.postid)
-            checkLikesBtn(holder.likeButton, holder.likes, post.postid)
+                                } else {
+                                    Toast.makeText(mContex, "Hulk", Toast.LENGTH_SHORT).show()
 
                                 }
+                            }
 
+                        }
+                    }.addOnFailureListener {
+                        Toast.makeText(mContex, "Hulk", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
+        }
 
 
 
     }
 
-    private fun checkLikesCount(likeBtn:ImageView,likes: TextView,postId:String) {
+    private fun checkLikesCount(likeBtn: ImageView, likes: TextView, postId: String) {
         val userUsing = firebaseAuth.currentUser?.uid
-        var post = db2.whereEqualTo("postid",postId)
+        var post = db2.whereEqualTo("postid", postId)
         post.get().addOnSuccessListener {
-            if (!it.isEmpty){
-                for (document in it){
+            if (!it.isEmpty) {
+                for (document in it) {
                     var like = document.reference.collection("Likes").get()
                     like.addOnSuccessListener {
                         if (it.count().toInt() > 0) {
                             likes.text = it.count().toString()
-                        }
-                        else if (it.isEmpty){
+                        } else if (it.isEmpty) {
                             likes.text = null
+                        }
                     }
+
+
                 }
-
-
-            }
             }
 
         }
     }
-    private fun checkLikesBtn(likeBtn:ImageView,likes: TextView,postId:String):Boolean{
+    private fun checkLikesBtn(likeBtn: ImageView, likes: TextView, postId: String) {
         val userUsing = firebaseAuth.currentUser?.uid
 
-        var post = db2.whereEqualTo("postid",postId)
+        var post = db2.whereEqualTo("postid", postId)
         post.get().addOnSuccessListener {
-            if (!it.isEmpty){
-                for (document in it){
+            if (!it.isEmpty) {
+                for (document in it) {
                     var like = document.reference.collection("Likes").whereEqualTo(
-                        "likedby",userUsing
+                        "likedby", userUsing
                     )
                     like.get().addOnSuccessListener {
                         for (document2 in it) {
                             var string = document2.get("likedby").toString()
                             if (it.contains(document2)) {
-                                    likeBtn.setImageResource(R.drawable.heart_clicked)
-                                    this.liked = true
+                                likeBtn.setImageResource(R.drawable.heart_clicked)
+                                checkLikesCount(likeBtn, likes, postId)
 
 
-
-                                } else {
+                            } else {
                                 likeBtn.setImageResource(R.drawable.heart_not_clicked)
-                                this.liked = false
-                                }
+                                this.lAU = "unliked"
 
                             }
+
                         }
                     }
-
                 }
 
             }
 
+        }
 
-        return this.liked
 
     }
-
-    private fun unlike(likeBtn:ImageView,likes: TextView,postId:String): Boolean {
+    private fun unlike(likeBtn: ImageView, likes: TextView, postId: String) {
         val userUsing = firebaseAuth.currentUser?.uid
-        var post = db2.whereEqualTo("postid",postId)
+        var post = db2.whereEqualTo("postid", postId)
         post.get().addOnSuccessListener {
-            if (!it.isEmpty){
-                for (document in it){
+            if (!it.isEmpty) {
+                for (document in it) {
                     var like = document.reference.collection("Likes").whereEqualTo(
-                        "likedby",userUsing
+                        "likedby", userUsing
                     )
                     like.get().addOnSuccessListener {
                         for (document2 in it) {
                             var string = document2.get("likedby").toString()
-                            if (it.contains(document2) && this.liked) {
-                                    document.reference.collection("Likes")
-                                        .document(document2.id).delete().addOnSuccessListener {
-                                            this.liked = false
-                                            likeBtn.setImageResource(R.drawable.heart_not_clicked)
-                                            checkLikesCount(likeBtn,likes,postId)
+                            if (it.contains(document2)) {
+                                document.reference.collection("Likes")
+                                    .document(document2.id).delete().addOnSuccessListener {
 
+                                        likeBtn.setImageResource(R.drawable.heart_not_clicked)
+                                        checkLikesCount(likeBtn, likes, postId)
+                                    }
+                            } else if (!it.contains(document2)) {
+                                likeBtn.setImageResource(R.drawable.heart_not_clicked)
 
-                                        }
-                                }
-                                else if (!it.contains(document2)) {
-                                    likeBtn.setImageResource(R.drawable.heart_not_clicked)
-                                    this.liked = false
-                                }
                             }
-
                         }
+
+                    }
                 }
             }
 
         }
-        return this.liked
-    }
 
-    private fun addLike(postId: String,likeBtn: ImageView) {
+    }
+    private fun addLike(postId: String, likeBtn: ImageView) {
         val userUsing = firebaseAuth.currentUser?.uid
         var likes = db2
         val posts = db2.whereEqualTo("postid", postId)
@@ -209,14 +234,11 @@ class PostAdapter(
                             "likedby" to userUsing,
                             "postid" to postId,
                             "publisher" to postInside.publisher,
-                            "postimage" to postInside.postimage
+                            "postimage" to postInside.postimage,
+                            "status" to "liked"
 
                         )
                         likes.document(idDoc).collection("Likes").document().set(mapId)
-                        this.liked = true
-                        if (likeBtn.equals(R.drawable.heart_not_clicked)){
-                            this.liked = false
-                        }
 
 
                     }
@@ -224,33 +246,10 @@ class PostAdapter(
             }
         }
     }
-    private fun checkDrawable(likebtn:ImageView): Boolean{
-
-        if (likebtn.resources.equals(R.drawable.heart_not_clicked)){
-            this.liked = false
-            likebtn.setImageResource(R.drawable.heart_not_clicked)
-           r = false
-        }
-
-        else if (likebtn.resources.equals(R.drawable.heart_clicked)){
-            r = true
-            likebtn.setImageResource(R.drawable.heart_clicked)
-        }
-        return r
-
-
-
-    }
-
     override fun getItemCount(): Int {
         return mPost.size
     }
-    private fun publisherInfo(
-        profileImage: CircleImageView,
-        userName: TextView,
-        publisher: TextView,
-        publisherID: String
-    ) {
+    private fun publisherInfo(profileImage: CircleImageView, userName: TextView, publisher: TextView, publisherID: String) {
         val users = db.document(publisherID)
         users.get().addOnSuccessListener {
             if (it.exists()) {
@@ -259,10 +258,10 @@ class PostAdapter(
                 userName.setText(user?.username)
                 publisher.setText(user?.name_completo)
 
+
             }
         }
     }
-
     private fun getDescriptions(description: TextView, publisherID: String) {
         val posts = db2
 
